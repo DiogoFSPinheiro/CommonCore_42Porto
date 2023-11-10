ce3d6348b67ef1f239de0d90afdd514679004419
DiogoFSPinheiro <143399294+DiogoFSPinheiro@users.noreply.github.com>
2023-11-10 06:40:44 +0000

    Update ft_lstlast.c

--- libf/ft_lstlast.c
+++ libf/ft_lstlast.c
@@ -2,7 +2,7 @@ t_list	*ft_lstlast(t_list	*list)
 {
 	if (lst == NULL)
 		return (NULL);
-  while (list) // sera que retorna o null ?
+ 	 while (list) // sera que retorna o null ?
 		list = list-> next;
 	return (list);
 }
