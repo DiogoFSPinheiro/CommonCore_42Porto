1556a60b7c31dd9dc579e724a15fe6df99d65220
DiogoFSPinheiro <143399294+DiogoFSPinheiro@users.noreply.github.com>
2023-11-10 07:35:15 +0000

    Update ft_lstadd_back.c

--- libf/ft_lstadd_back.c
+++ libf/ft_lstadd_back.c
@@ -3,9 +3,12 @@ void ft_lstadd_back(t_list **lst, t_list *new)
       if (lst == NULL && new == NULL)
 		    return;
       if (lst == NULL)
-        *lst = new
-      while (lst)
-		    lst = lst-> next;
+      	{
+        	*lst = new
+		return;
+	}
+	while (lst)
+	    lst = lst-> next;
 	  
       lst -> next = new;
 }
