e8955fb83363ff8138a300d1c967af5ca15cba1b
DiogoFSPinheiro <143399294+DiogoFSPinheiro@users.noreply.github.com>
2023-11-10 03:07:33 +0000

    Create ft_lstnew.c

---
+++ libf/ft_lstnew.c
@@ -0,0 +1,12 @@
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	if (!list)
+    return (0);
+	list -> content = content;
+	list -> next = NULL;
+	return (list);
+}
