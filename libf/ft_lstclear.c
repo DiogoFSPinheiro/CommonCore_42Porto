d1dc3beb8efa64c7ac016a61a0476a90287b6e93
DiogoFSPinheiro <143399294+DiogoFSPinheiro@users.noreply.github.com>
2023-11-14 15:11:31 +0000

    Delete libf directory

--- libf/ft_lstclear.c
+++
@@ -1,8 +0,0 @@
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	if (!*lst)
-		return ;
-	while (templist -> next)
-    ft_lstdelone(templist, del);
-	*lst = NULL;
-}
--- libf/ft_lstlast.c
+++
@@ -1,8 +0,0 @@
-t_list	*ft_lstlast(t_list	*list)
-{
-	if (lst == NULL)
-		return (NULL);
- 	 while (list) // sera que retorna o null ?
-		list = list-> next;
-	return (list);
-}
