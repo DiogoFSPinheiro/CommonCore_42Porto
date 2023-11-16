ee8ef96be35a5ba210d0e3d95bf2c4a9699d0cbc
DiogoFSPinheiro <143399294+DiogoFSPinheiro@users.noreply.github.com>
2023-11-16 03:51:36 +0000

    Update ft_putnbr_ptr.c

--- printf/ft_putnbr_ptr.c
+++ printf/ft_putnbr_ptr.c
@@ -32,3 +32,43 @@ int	ft_putnbr_ptr(unsigned long long ptr, char *base, int fd, int c)
 		c += ft_putchar_fd(base[ptr], fd);
 	return (c);
 }
+// cenas alteradas nem sei como funciona
+
+int print_ptr(unsigned long long ptr, int fd)
+{
+    char *base;
+    int c;
+    c = 0;
+    base = "0123456789abcdef";
+    
+    if (ptr >= 16)
+	{
+		c = print_ptr(ptr / 16, fd);
+		ptr = ptr % 16;
+	}
+	if (ptr < 16)
+		c += ft_putchar_fd(base[ptr], fd);
+	return (c);
+}
+
+
+int	ft_putnbr_ptr(unsigned long long ptr, int fd)
+{
+	unsigned long long	len;
+	int c;
+	
+	c = 0;
+
+	if (ptr == 0)
+	{
+		c = ft_putstr_fd("(nil)", 1);
+		return (c);
+	}
+	c = ft_putstr_fd("0x", 1);
+	c += print_ptr(ptr, fd);
+
+	return (c);
+}
+
+	return (ft_putnbr_ptr(va_arg(args, int), 1));
+
