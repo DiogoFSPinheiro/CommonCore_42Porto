81e8e25c69f46ec2770fa88e80b429c0b4b1e869
DiogoFSPinheiro <143399294+DiogoFSPinheiro@users.noreply.github.com>
2024-02-05 18:13:34 +0000

    Add files via upload

---
+++ Push_swap/Push_swap.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int	ft_isdigit(int c)
+{
+	if (c < '0' || c > '9')
+		return (0);
+	return (1);
+}
+
+int	is_string_digit(char *str)
+{
+	int i = 0;
+
+	// Check if the string is empty
+	if (str[0] == '\0')
+		return (0);
+
+	// Check each character
+	while (str[i] != '\0')
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	int c = 1;
+	
+	if (argc < 2)
+		return write(1, "Error\n", 6);
+	else
+	{
+		while (c < argc)
+		{
+			if (!is_string_digit(argv[c]))
+				return write(1, "Error\n", 6);
+			printf("numero = %s\n", argv[c]);
+			c++;
+		}
+	}
+	return 0;
+}
+
